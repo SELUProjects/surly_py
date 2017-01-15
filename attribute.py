012a97b40234e13e87b47d13b262eaae3090d365
billy <wewill147@gmail.com>
2017-01-14 22:43:06 -0600

    mostly empy files

---
+++ attribute.py
@@ -0,0 +1,5 @@
+class Attribute:
+
+	def __init__(self, name, dataType):
+		self.name = name
+		self.dataType = dataType
---
+++ tuple.py
@@ -0,0 +1,6 @@
+from attribute import Attribute
+
+class Tuply:
+	"""docstring for Tuple"""
+	def __init__(self, arg):
+		self.arg = arg
